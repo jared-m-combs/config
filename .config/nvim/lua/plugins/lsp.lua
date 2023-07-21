81b0687dea87e901a8d10ceea85ec25b0cd77900
Jared Combs <9115033+jared-m-combs@users.noreply.github.com>
2023-07-20 23:41:10 -0400

    init

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = {
+      sources = {
+        require("null-ls").builtins.formatting.stylua,
+        require("null-ls").builtins.formatting.shfmt,
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- autoformat = false,
+      servers = {
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                buildScripts = {
+                  enabled = true,
+                },
+              },
+              imports = {
+                granularity = {
+                  group = "module",
+                },
+              },
+              checkOnSave = {
+                allFeatures = true,
+                overrideCommand = {
+                  "cargo",
+                  "clippy",
+                  "--workspace",
+                  "--message-format=json",
+                  "--all-targets",
+                  "--all-features",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
